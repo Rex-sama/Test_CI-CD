a66733ae2e7a093aa31793fa0087ed5aed508a47
Rohit <rohit.kadu@ketto.org>
2024-05-01 11:28:16 +0530

    fix

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ app.get('/api/data',(req,res)=>{
     res.json({message:"Hello from server!"})
 })
 
-const PORT = 5000
+const PORT = 5000 
 
 app.listen(PORT,()=>{
     console.log("Server is running on PORT " + PORT)
